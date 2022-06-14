10e4fbcd31f6921e97015673e068aad928170d28
Saileela P <sp@zuelligpharma.com>
2022-06-14 22:15:28 +0530

    setting up the git repo for go bookstore project

---
+++ pkg/routes/bookstore-rotes.go
@@ -0,0 +1,12 @@
+package routes
+
+import (
+	// "/go-bookstore/pkg/controllers"
+
+	"github.com/gorilla/mux"
+)
+
+var registerBookStoreRoutes = func(router *mux.Router) {
+	// router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
+
+}
